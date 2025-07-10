cb16db3f0c5b8a864d43f82d88d734efc75094e3
Deilons <apolo4748@email.com>
2025-07-10 13:13:04 -0500

    Feat: Add AppDbContext for database interaction

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using JwtAuthAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
